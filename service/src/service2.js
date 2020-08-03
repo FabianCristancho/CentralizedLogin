74089ee324e09e57d4af20abfc64d580b745e6fb
FabianCristancho <37461628+FabianCristancho@users.noreply.github.com>
2020-08-03 14:28:06 -0500

    New service

---
+++ service/src/service2.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const axios = require('axios');
+const ip = require('ip');
+
+const app = express();
+const hostMiddleware = process.argv[2]
+const port = process.argv[3]
+
+app.get('/', (req, res) => {
+     res.send("server");
+})
+
+app.get('/receiveReq', (req, res) => {
+     console.log("Recibida la solicitud del middleware");
+     console.log(isPalindrome(req.query.word));
+     res.send(isPalindrome(req.query.word));
+     sendLog(res);
+});
+
+function sendLog(res){
+     axios.get('http://'+hostMiddleware+':3000/log',{
+          params: {
+               date: new Date(),
+               server:ip.address() +":" +port,
+               currentTime: process.hrtime(),
+               status: res.statusCode,
+               level: 'OK'
+          }
+     }).then(res =>{
+          // res.connection.destroy();
+     }).catch(error => {
+          console.log('middleware desconectado')
+          // console.log(error);
+     });
+}
+
+function isPalindrome(word){
+     var evaluateWord = word.replace(/ /g, "").toLowerCase();
+     var i = 0;
+     var j = evaluateWord.length - 1;
+     while(i <= j){
+          if(evaluateWord[i] !== evaluateWord[j]){
+               return "'" +word +"'" +" no es palíndroma";
+          }
+          i++;
+          j--;
+     }
+     return "'" +word +"'" +" es palíndroma";
+}
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
